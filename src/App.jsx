8d43062f6994e593277c655a9c20bdb84a248cb5
Kyle <povey.kyle@gmail.com>
2023-11-18 11:24:52 +0000

    set up internal json-server for solutions and renderd them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+import "./index.css";
 
 function App() {
+  const [solution, setSolution] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        // random number between 0 and the length of the array
+
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+
+        setSolution(randomSolution);
+      });
+  }, [setSolution]);
+
+  console.log(solution);
+
   return (
-    <>
-      <p>test</p>
-    </>
+    <div className="App">
+      <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is: {solution.word}</div>}
+    </div>
   );
 }
 
